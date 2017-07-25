18932be3124d88c67c14838216e0f8e8611571d1
Benjamin Lau <blau@salesforce.com>
2017-07-25 11:56:49 -0700

    DirDecryptor initial checkin

---
+++ src/com/company/caesars/pgp/DirDecryptor.java
@@ -0,0 +1,44 @@
+package com.company.caesars.pgp;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+//Decryptor *pgp files in a directory
+public class DirDecryptor {
+
+	public static void main(String[] args) throws IOException {
+		
+		//setup basic decryption criteria
+		InputStream keyIn = new FileInputStream("keys/PGP/st/private.asc");
+		char[] password = "Ri0Vegas!".toCharArray();
+		
+		String inFile = "data/GuestContacts/gst_pref_glba.zip.pgp";
+		
+		decryptFile(inFile, getOutFile(inFile), keyIn, password);
+	}
+	
+	public static String getOutFile(String inFile) {
+		if (inFile.endsWith(".pgp") || inFile.endsWith(".PGP")) {
+			return inFile.substring(0, inFile.length()-4);
+		} else {
+			return null;
+		}
+	}
+	
+	public static void decryptFile(String inFile, String outFile, InputStream keyIn, char[] password) throws IOException {
+		
+		InputStream in = new FileInputStream(inFile);
+		OutputStream out = new FileOutputStream(outFile);
+			
+		try {
+			Decryptor.decryptFile(in, out, keyIn, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		out.close();
+	}
+}
